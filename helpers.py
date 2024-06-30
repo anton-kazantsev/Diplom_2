1dce883264e124115b085c35b96212af30db1203
Anton Kazantsev <baksfire@yandex.ru>
2024-07-01 00:43:57 +0500

    first commit

--- helpers.py
+++
@@ -1,3 +0,0 @@
-burger1 = {'ingredients': ['61c0c5a71d1f82001bdaaa6d', '61c0c5a71d1f82001bdaaa70']}
-burger2 = {'ingredients': []}
-burger3 = {'ingredients': ['61c0c5a71d1f82001bd6d', '61c0c5a71d1f82001bd33333']}
