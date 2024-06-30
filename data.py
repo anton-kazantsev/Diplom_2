1386b69411848a47e2b7cabf48477285040b8619
Anton Kazantsev <baksfire@yandex.ru>
2024-07-01 00:51:21 +0500

    first commit

--- data.py
+++
@@ -1,7 +0,0 @@
-class DataUrls:
-    BASE_URL = 'https://stellarburgers.nomoreparties.site/'
-    CREATE_USER = 'https://stellarburgers.nomoreparties.site/api/auth/register'
-    LOGIN_USER = 'https://stellarburgers.nomoreparties.site/api/auth/login'
-    CHANGING_USER = 'https://stellarburgers.nomoreparties.site/api/auth/user'
-    CREATE_ORDER = 'https://stellarburgers.nomoreparties.site/api/orders'
-    ALL_ORDER = 'https://stellarburgers.nomoreparties.site/api/orders/all'
